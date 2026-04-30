cf3c4e25d4375bab9a263c60e6db30de6de317f6
ZZSECCIAC <claudio.seccia@vodafone.com>
2026-04-30 17:58:07 +0200

    feat: exercise one - using props

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,36 @@ import Footer from "./components/Footer";
 import "./App.scss";
 
 function App() {
+  const menu = [
+    { name: "Home", link: "#" },
+    { name: "Contatti", link: "#" },
+  ];
+  const textLogo = "Logo";
+  const posts = [
+    {
+      title: "Post 1",
+      text: "Testo di esempio per la prima card...",
+    },
+    {
+      title: "Post 2",
+      text: "Testo di esempio per la seconda card...",
+    },
+    {
+      title: "Post 3",
+      text: "Testo di esempio per la terza card...",
+    },
+  ];
+  console.log(posts);
+  //usare map per iterare i post
   return (
     <>
-      <Navbar />
+      <Navbar menu={menu} textLogo={textLogo} />
       <Hero />
 
       <main className="container">
         <div className="articles">
           <div className="cards">
+            {/* posts.map */}
             <Card title="Post 1" text="Testo di esempio per la prima card..." />
             <Card
               title="Post 2"
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,10 @@
-export default function Navbar() {
+// export default function Navbar(props) {
+//console.log(props.menu);
+//const menu = props.menu;
+export default function Navbar({ menu, textLogo }) {
+  console.log(menu);
+  console.log(textLogo);
+  //usare map per iterare le voci del menu
   return (
     <nav className="navbar">
       <div className="logo">Logo</div>
